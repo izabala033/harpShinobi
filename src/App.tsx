2b1fbe308f6c5b1b9b0da504cc8a468c60da80e1
Iker <iker13z@gmail.com>
2025-06-14 12:25:37 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Circle from "./Circle";
+import Harmonica from "./Harmonica";
+import Settings from "./Settings";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/circle" element={<Circle />} />
+        <Route path="/harmonica" element={<Harmonica />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/" element={<Navigate to="/harmonica" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Circle.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Circle() {
+  return <></>;
+}
+
+export default Circle;
